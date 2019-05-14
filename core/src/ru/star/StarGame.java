4c414c18886103a6b6e9d7afa6bfd72e630e5637
rrmice <rrmice@yandex.ru>
2019-05-14 21:13:27 +0300

    1stCom

--- core/src/ru/star/StarGame.java
+++ core/src/ru/star/StarGame.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import javax.swing.plaf.synth.Region;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
