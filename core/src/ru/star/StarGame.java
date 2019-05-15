ef167d5d73d8db3dbd7a5168c7f70be35e9a67a8
rrmice <rrmice@yandex.ru>
2019-05-15 17:26:58 +0300

    Add background

--- core/src/ru/star/StarGame.java
+++ core/src/ru/star/StarGame.java
@@ -11,12 +11,12 @@ import javax.swing.plaf.synth.Region;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.jpg");
 	}
 
 	@Override
@@ -24,13 +24,13 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+        background.dispose();
 	}
 }
